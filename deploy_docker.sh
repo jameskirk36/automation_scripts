b7093fa1da5211204da560d7f84ea0081cb79378
James Kirk <james.kirk84@gmail.com>
2015-08-03 15:18:42 +0000

    now pulls specific version

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,4 +1,10 @@
 echo "Deploying docker containers..."
-bundle exec ruby sshwrapper.rb "cd $1; git pull"
-bundle exec ruby sshwrapper.rb "cd $1; docker-compose pull"
-bundle exec ruby sshwrapper.rb "cd $1; docker-compose up -d"
+command="
+  cd $1;\
+  git reset --hard;\
+  git pull;\
+  sed -i \"s_nice/mimir_&:$2_\" docker-compose.yml;\
+  docker-compose pull;\
+  docker-compose up -d;\
+"
+bundle exec ruby sshwrapper.rb "$command"
