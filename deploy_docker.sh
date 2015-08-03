242ae3b8cae9e421941599e1df53a338e4ada447
James Kirk <james.kirk84@gmail.com>
2015-08-03 12:45:14 +0000

    pull on deploy

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,2 +1,4 @@
 echo "Deploying docker containers..."
+bundle exec ruby sshwrapper.rb "cd $1; git pull"
+bundle exec ruby sshwrapper.rb "cd $1; docker-compose pull"
 bundle exec ruby sshwrapper.rb "cd $1; docker-compose up -d"
