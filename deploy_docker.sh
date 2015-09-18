16159cae2c2c4bbcffdadd185a24a9ae7f3d5ef4
James Kirk <james.kirk84@gmail.com>
2015-09-18 13:39:12 +0000

    teardown first

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,11 +1,11 @@
 echo "Deploying docker containers..."
 
 REVISION=$GO_REVISION_LD_KB_QS
-REPO_URL=$2
 TARGET_DIR=$1
 
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR"
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git clone $REPO_URL ."
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose stop"
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git pull"
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git reset --hard $REVISION"
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose pull"
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose up -d"
