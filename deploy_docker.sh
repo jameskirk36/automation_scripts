b8642b0d66297ba7fbb35c87e21309b28eca3861
James Kirk <james.kirk84@gmail.com>
2015-09-21 10:11:10 +0000

    actually failing correctly

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -3,9 +3,9 @@ echo "Deploying docker containers..."
 REVISION=$GO_REVISION_LD_KB_QS
 TARGET_DIR=$1
 
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR" || exit(1)
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose stop" || exit(1)
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git pull" || exit(1)
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git reset --hard $REVISION" || exit(1)
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose pull" || exit(1)
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose up -d" || exit(1)
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR" || exit $?
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose stop" || exit $?
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git pull" || exit $?
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git reset --hard $REVISION" || exit $?
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose pull" || exit $?
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose up -d" || exit $?
--- sshwrapper.rb
+++ sshwrapper.rb
@@ -5,24 +5,49 @@ username = ENV["USERNAME"]
 password = ENV["PASSWORD"] 
 command = ARGV[0]
 
-class Job
-  attr_accessor :stdout
-  attr_accessor :stderr
-end
-
-job = Job.new
 puts "#{username}@#{host}"
+
+def ssh_exec!(ssh, command)
+  stdout_data = ""
+  stderr_data = ""
+  exit_code = nil
+  exit_signal = nil
+  ssh.open_channel do |channel|
+    channel.exec(command) do |ch, success|
+      unless success
+        abort "FAILED: couldn't execute command (ssh.channel.exec)"
+      end
+
+      channel.on_data do |ch,data|
+        stdout_data+=data
+      end
+  
+      channel.on_extended_data do |ch,type,data|
+        stderr_data+=data
+      end
+  
+      channel.on_request("exit-status") do |ch,data|
+        exit_code = data.read_long
+      end
+  
+      channel.on_request("exit-signal") do |ch, data|
+        exit_signal = data.read_long
+      end
+    end
+  end
+  ssh.loop
+  return stdout_data, stderr_data, exit_code, exit_signal
+end
 
 Net::SSH.start(host, username, password: password)  do |ssh|
   puts "running command: #{command}"
-  job.stdout  = ""
-  job.stderr = ""
-  ssh.exec! command do |channel, stream, data|
-    job.stdout << data if stream == :stdout
-    job.stderr << data if stream == :stderr
+  stdout_data, stderr_data, exit_code, exit_signal = ssh_exec!(ssh, command) 
+  unless stderr_data.empty?
+    puts stderr_data
   end
+  unless stdout_data.empty?
+    puts stdout_data
+  end
+  puts "command finished with exit code #{exit_code}"
+  exit(exit_code)
 end
-puts "stderr:"
-puts job.stderr
-puts "stdout:"
-puts job.stdout
