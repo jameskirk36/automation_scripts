00ed926d72dbd460ccca6a0823e19797eb7f04b3
James Kirk <james.kirk84@gmail.com>
2015-09-21 09:45:38 +0000

    failing correctly

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -3,9 +3,9 @@ echo "Deploying docker containers..."
 REVISION=$GO_REVISION_LD_KB_QS
 TARGET_DIR=$1
 
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR"
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose stop"
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git pull"
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git reset --hard $REVISION"
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose pull"
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose up -d"
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR" || exit(1)
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose stop" || exit(1)
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git pull" || exit(1)
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git reset --hard $REVISION" || exit(1)
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose pull" || exit(1)
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose up -d" || exit(1)
