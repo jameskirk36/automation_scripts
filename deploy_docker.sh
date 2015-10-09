c2e30447f22f2fd39c8742b611de3c892fb1bf2f
James Kirk <james.kirk84@gmail.com>
2015-10-09 11:58:03 +0100

    adding editor password

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,6 +1,7 @@
 echo "Deploying docker containers..."
 
 REVISION=$GO_PIPELINE_LABEL
+editorpass=$EDITORPASSWORD
 TARGET_DIR=$1
 
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR" || exit $?
@@ -8,4 +9,4 @@ bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose stop" || exit $?
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git pull" || exit $?
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git reset --hard $REVISION" || exit $?
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose pull" || exit $?
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose up -d" || exit $?
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; EDITORPASSWORD=$editorpass docker-compose up -d" || exit $?
