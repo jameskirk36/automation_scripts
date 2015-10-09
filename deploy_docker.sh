cfaa36df5338115ac605de81a6d1f33dd1494969
James Kirk <james.kirk84@gmail.com>
2015-10-09 14:53:14 +0100

    manually removing auth container, sigh

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -5,7 +5,8 @@ editorpass=$EDITORPASSWORD
 TARGET_DIR=$1
 
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR" || exit $?
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose stop" || exit $?
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose stop" || exit $? 
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker rm -f ldkbqs_auth_1" || exit $?
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git pull" || exit $?
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git reset --hard $REVISION" || exit $?
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose pull" || exit $?
