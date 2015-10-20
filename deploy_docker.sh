08b4dbd26f00006b622b402f7a02b424ef0ffe87
James Kirk <james.kirk84@gmail.com>
2015-10-20 10:16:17 +0100

    remove auth container is optional

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -6,7 +6,7 @@ TARGET_DIR=$1
 
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR" || exit $?
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose stop" || exit $? 
-bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker rm -f ldkbqs_auth_1" || exit $?
+bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker rm -f ldkbqs_auth_1"
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git pull" || exit $?
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; git reset --hard $REVISION" || exit $?
 bundle exec ruby sshwrapper.rb "cd $TARGET_DIR; docker-compose pull" || exit $?
